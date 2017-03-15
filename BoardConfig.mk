394278cf9050c7989687122ecb2193b89ab25162
vibhoothiiaanand <vibhoothiiaanand@gmail.com>
2017-03-15 15:26:02 +0000

    Make dexpropt false as Default

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,7 +164,7 @@ USE_SENSOR_MULTI_HAL := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT := false
    endif
   endif
 endif

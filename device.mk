af674624578692a5d993744ce00deaa207a78794
Luca Stefani <luca020400@lineageos.org>
2017-09-01 18:59:17 -0400

    op2: Build Bluetooth HAL
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -57,6 +57,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+  android.hardware.bluetooth@1.0-impl
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080

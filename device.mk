b3407f532ab8d21f076afa212e4feb6ecfe45631
Shreesha <shreeshu10@gmail.com>
2017-09-01 20:12:59 -0400

    op2: import HWUI configs from angler
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -68,8 +68,20 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     device/oneplus/oneplus2/extras/bootanimation/bootanimation.zip:system/media/bootanimation.zip   
  
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
 
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard

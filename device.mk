a30f8b400a2e7c6c785c4145b85cb8c97fb6a2f4
Shreesha <shreeshu10@gmail.com>
2017-09-04 13:44:16 -0400

    op2: sensor: move hals.conf to vendor
    * inlcude sensor wrapper which got removed

--- device.mk
+++ device.mk
@@ -300,10 +300,11 @@ PRODUCT_BOOT_JARS += telephony-ext
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    sensors.ssc.wrapper
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf
+    $(LOCAL_PATH)/configs/sensors/hals.conf:system/vendor/etc/sensors/hals.conf
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \

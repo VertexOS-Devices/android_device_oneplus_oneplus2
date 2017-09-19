e81fb08b68271b3af68230f33a06f6bcaf8ce5a8
thecrazyskull <anaskarbila@aospa.co>
2017-09-19 10:01:02 +0530

    op2: deprecate CameraParametersExtra.h
    Conflicts:
    	libop2_cam/op2_cam.c

--- libop2_cam/op2_cam.c
+++ libop2_cam/op2_cam.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 Lineage Android
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,4 @@
  * limitations under the License.
  */
 
-#define CAMERA_PARAMETERS_EXTRA_C \
-const char CameraParameters::KEY_APP_MASK[] = "app-mask"; \
-const char CameraParameters::PIXEL_FORMAT_YUV420SP_NV21[] = "nv21";
-
-#define CAMERA_PARAMETERS_EXTRA_H \
-    static const char KEY_APP_MASK[]; \
-    static const char PIXEL_FORMAT_YUV420SP_NV21[];
+const char _ZN7android16CameraParameters12KEY_APP_MASKE[] = "app-mask";
